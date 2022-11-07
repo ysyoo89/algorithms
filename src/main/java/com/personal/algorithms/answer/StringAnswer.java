24686ecf4a260d71662b112d9091e9d08ff24c73
ysyoo89 <ysyoo89@kakao.com>
2022-11-07 23:02:08 +0900

    String 알고리즘 문제 풀이 (난이도 : easy)

--- src/main/java/com/personal/algorithms/answer/StringAnswer.java
+++ src/main/java/com/personal/algorithms/answer/StringAnswer.java
@@ -1,5 +1,8 @@
 package com.personal.algorithms.answer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class StringAnswer {
 
     public static int mostWordsFound(String[] sentences) {
@@ -27,5 +30,21 @@ public class StringAnswer {
         result = result.replace("()", "o");
         result = result.replace("(al)", "al");
         return result;
+    }
+
+    public static List<String> cellsInRages(String s) {
+        List<String> ans = new ArrayList<>();
+        StringBuilder curAns = new StringBuilder();
+        for(int i = s.charAt(0); i<=s.charAt(3); i++)
+        {
+            for(int f = s.charAt(1)-'0'; f<=s.charAt(4)-'0'; f++)
+            {
+                curAns.append((char)i);
+                curAns.append(f);
+                ans.add(curAns.toString());
+                curAns.delete(0,2);
+            }
+        }
+        return ans;
     }
 }
