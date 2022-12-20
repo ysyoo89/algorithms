9cb38e979043dd90ea4647ec1243e48f8ec0b9c8
ysyoo89 <ysyoo89@kakao.com>
2022-12-20 19:37:37 +0900

    Programmers 알고리즘 문제 풀기 (난이도 : level 1)

--- src/main/java/com/personal/programmers/answer/ProgrammersAnswer.java
+++ src/main/java/com/personal/programmers/answer/ProgrammersAnswer.java
@@ -2,6 +2,7 @@ package com.personal.programmers.answer;
 
 import java.text.SimpleDateFormat;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class ProgrammersAnswer {
 
@@ -322,7 +323,22 @@ public class ProgrammersAnswer {
     }
 
     public static String[] stringSort(String[] strings, int n) {
-        String[] result = {};
-        return new String[0];
+        for(int i=0; i<strings.length-1; i++){
+            for(int k=i+1; k<strings.length;k++){
+                if(strings[i].charAt(n)>strings[k].charAt(n)){
+                    String tmp = strings[i];
+                    strings[i]= strings[k];
+                    strings[k]=tmp;
+                }
+                else if(strings[i].charAt(n)==strings[k].charAt(n)){
+                    if(strings[i].compareTo(strings[k]) >0){
+                        String tmp = strings[i];
+                        strings[i]= strings[k];
+                        strings[k]=tmp;
+                    }
+                }
+            }
+        }
+        return strings;
     }
 }
