3c94af6d4bc81b86be56f16fa50a7f3c2d009f30
ysyoo89 <ysyoo89@kakao.com>
2023-03-01 20:52:22 +0900

    문제 풀이

--- src/main/java/com/personal/programmers/answer/ProgrammersAnswerLevel2.java
+++ src/main/java/com/personal/programmers/answer/ProgrammersAnswerLevel2.java
@@ -442,6 +442,7 @@ public class ProgrammersAnswerLevel2 {
 
 	public static int sortRoad(int[][] maps) {
 		int[][] check = new int[maps.length][maps[0].length];
+		// TODO 하나씩 체크할 것
 		return 0;
 	}
 }
