306fe622b1fdd1d9417c532f59775d33597a9b63
ysyoo89 <ysyoo89@kakao.com>
2023-04-12 21:49:52 +0900

    문제 풀이 추가

--- src/main/java/com/personal/programmers/answer/ProgrammersAnswerLevel2.java
+++ src/main/java/com/personal/programmers/answer/ProgrammersAnswerLevel2.java
@@ -505,5 +505,52 @@ public class ProgrammersAnswerLevel2 {
 		}
 		return result;
 	}
+	
+	class Homework {
+        String name;
+        int start, playtime;
+        public Homework(String name, String start, String playtime){
+            this.name = name;
+            this.start = parseTime(start);
+            this.playtime = Integer.parseInt(playtime);
+        }
+        private int parseTime(String time){
+            String[] t = time.split(":");
+            return Integer.parseInt(t[0]) * 60 + Integer.parseInt(t[1]);
+        }
+    }
+	public String[] solution(String[][] plans) {
+        List<String> answer = new ArrayList<>();
+        Queue<Homework> remain = new PriorityQueue<>((h1, h2) -> h1.start - h2.start);
+        Deque<Homework> task = new ArrayDeque<>();
+        for(String[] plan : plans){
+            remain.offer(new Homework(plan[0], plan[1], plan[2]));
+        }
+        int currTime = remain.peek().start;
+        task.add(remain.poll());
+        while(!remain.isEmpty()){
+            Homework next;
+            if(remain.peek().start < currTime+task.peekLast().playtime){
+                // 새 과제 시작전에 진행중인 작업을 끝내지 못함
+                next = remain.poll();
+                task.peekLast().playtime -= next.start - currTime;
+                currTime = next.start;
+                task.add(next);
+            }
+            else{
+                // 진행중인 작업 끝
+                currTime += task.peekLast().playtime;
+                answer.add(task.pollLast().name);
+            }
+            if(task.isEmpty()) {
+                currTime = remain.peek().start;
+                task.add(remain.poll());
+            }
+        }
+        while(!task.isEmpty()){
+            answer.add(task.pollLast().name);
+        }
+        return answer.toArray(String[]::new);
+    }
 }
 
