3f87f34dc8d9ba6d50aa668146f393ca4bf13a1a
ysyoo89 <ysyoo89@kakao.com>
2023-10-10 17:48:05 +0900

    알고리즘 공부

--- src/main/java/com/personal/mystudy/Answer.java
+++ src/main/java/com/personal/mystudy/Answer.java
@@ -38,37 +38,34 @@ public class Answer {
     }
 
     public static int[] triangle(int n) {
-        int[][] result = new int[n][n];
-        int number = 1;
-        int i = 0;
-        int j = 0;
-        boolean isComplete = false;
-        boolean isMove = true;
-        while(!isComplete) {
-            if (result[i][j] > 0) {
-                if (isMove) {
-                    j++;
+        int[] answer = new int[(n*(n+1))/2];
+        int[][] matrix = new int[n][n];
+
+        int x = -1, y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (i % 3 == 0) {
+                    ++x;
+                } else if (i % 3 == 1) {
+                    ++y;
+                } else if (i % 3 == 2) {
+                    --x;
+                    --y;
                 }
-            }
-            result[i][j] = number;
-            number++;
-            if (isMove && i == n - 1) {
-                j++;
-                if (j == n) {
-                    isMove = false;
-                    i--;
-                    j -= 2;
-                }
-            } else if (!isMove) {
-                i--;
-                j--;
-                if (result[i][j] > 0) {
-                    isMove = true;
-                    i += 2;
-                    j++;
-                }
+                matrix[x][y] = num++;
             }
         }
-        return new int[0];
+
+        int k = 0;
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 0) break;
+                answer[k++] = matrix[i][j];
+            }
+        }
+
+        return answer;
     }
 }
