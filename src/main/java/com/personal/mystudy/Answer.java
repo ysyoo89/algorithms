cf7baef0c9df5847cd8f7a7b2c230b428b8e95b3
ysyoo89 <ysyoo89@kakao.com>
2023-11-01 14:24:56 +0900

    알고리즘 공부

--- src/main/java/com/personal/mystudy/Answer.java
+++ src/main/java/com/personal/mystudy/Answer.java
@@ -587,8 +587,42 @@ public class Answer {
     }
 
     public static int rank(int n, int[][] results) {
+        boolean[][] wins = new boolean[n+1][n+1];
+        boolean[][] loses = new boolean[n+1][n+1];
+        int answer = 0;
+        for (int[] result : results) {
+            wins[result[0]][result[1]] = true;
+            loses[result[1]][result[0]] = true;
+        }
 
-        return 0;
+        for (int i = 1; i < n+1; i++ ) {
+            for (int j = 1; j < n+1; j++) {
+                for (int k = 1; k < n+1; k++) {
+                    if (wins[i][k] && wins[k][j]) {
+                        wins[i][j] = true;
+                        loses[j][i] = true;
+                    }
+                    if (loses[i][k] && loses[k][j]) {
+                        wins[j][i] = true;
+                        loses[i][j] = true;
+                    }
+                }
+            }
+        }
+
+        for (int i =1; i < n+1; i++) {
+            int number = 0;
+            for (int j = 1; j < n+1; j++) {
+                if (loses[i][j] || wins[i][j]) {
+                    number++;
+                }
+            }
+            if (number == n - 1) {
+                answer++;
+            }
+        }
+
+        return answer;
     }
 
     public static String exam(int n, int k) {
