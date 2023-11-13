2388ee8b192dbd85b80db69b6c25f4d543d2a2d7
ysyoo89 <ysyoo89@kakao.com>
2023-11-13 18:53:18 +0900

    알고리즘 공부

--- src/main/java/com/personal/mystudy/Answer.java
+++ src/main/java/com/personal/mystudy/Answer.java
@@ -720,6 +720,36 @@ public class Answer {
     }
 
     public static String bus(int n, int t, int m, String[] timetable) {
-        return null;
+        String answer = "";
+        int[] times = new int[timetable.length];
+        for (int i = 0; i < timetable.length; i++) {
+            String[] timeAndMinute = timetable[i].split(":");
+            times[i] = Integer.parseInt(timeAndMinute[0]) * 60 + Integer.parseInt(timeAndMinute[1]);
+        }
+        Arrays.sort(times);
+        int busTimeLimit = 9 * 60;
+        int index = 0;
+        int person = 0;
+        for (int i = 0; i < n; i++) {
+            person = 0;
+            for (int j = index; j < times.length; j++) {
+                if (times[j] <= busTimeLimit) {
+                    person++;
+                    index++;
+                    if (person == m) {
+                        break;
+                    }
+                }
+            }
+            busTimeLimit += t;
+        }
+        int result = 0;
+        if (person < m) {
+            result = busTimeLimit - t;
+        } else {
+            result = times[index - 1] - 1;
+        }
+        answer = ((result / 60) < 10 ? "0" + (result / 60) :  (result / 60)) + ":" + ((result % 60) < 10 ? "0" + (result % 60) :  (result % 60));
+        return answer;
     }
 }
